8a85e556795f75f741ea6141d44d99dc4fb70cf1
necrokamo <necrokamo@hotmail.com>
2016-06-22 22:30:22 -0500

    Clase 22/06/2016: Razor - Enrutamientos

--- WebDeveloper/App_Start/RouteConfig.cs
+++ WebDeveloper/App_Start/RouteConfig.cs
@@ -14,10 +14,19 @@ namespace WebDeveloper
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Razor",
+                url: "Razor/{id}",
+                defaults: new { controller = "Razor", action = "Product", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
+
         }
     }
 }
